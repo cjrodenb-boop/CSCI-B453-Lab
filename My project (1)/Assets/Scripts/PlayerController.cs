1dcf5f5367266a468b09722fde933ce573aa5172
cjbytheocean <cjrodenb@iu.edu>
2026-01-15 19:31:34 -0500

    lab 1 changes

--- My project (1)/Assets/Scripts/PlayerController.cs
+++ My project (1)/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] float sensitivity;
+    [SerializeField] float sprintSpeed;
+
     private float moveFB;
     private float moveLR;
     private float rotX;
@@ -21,6 +26,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        float movementSpeed = speed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = sprintSpeed;
+        }
+        else
+        {
+            movementSpeed = speed;
+        }
+
+        moveFB = Input.GetAxis("Vertical") * movementSpeed;
+        moveLR = Input.GetAxis("Horizontal") * movementSpeed;
+        rotX = Input.GetAxis("Mouse X") * sensitivity;
+        rotY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        rotY = Mathf.Clamp(rotY, -60f, 60f);
+
+        Vector3 movement = new Vector3(moveLR, 0, moveFB).normalized * movementSpeed;
+        transform.Rotate(0, rotX, 0);
+
+        playerCam.transform.localRotation = Quaternion.Euler(rotY, 0, 0);
+        movement = transform.rotation * movement;
+
+        cc.Move(movement * Time.deltaTime);
         
     }
 }
